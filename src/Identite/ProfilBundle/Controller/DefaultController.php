5ad65e9b2f57bd8e2d6a2efa06fc825479234b66
Laurent <laurent.cardon@jsb.be>
2014-03-03 13:45:57 +0100

    Supression du bundle login

--- src/Identite/ProfilBundle/Controller/DefaultController.php
+++ src/Identite/ProfilBundle/Controller/DefaultController.php
@@ -6,8 +6,38 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function loginAction()
     {
-        return $this->render('IdentiteProfilBundle:Default:index.html.twig', array('name' => $name));
+		    	
+		/*$request = $this->getRequest();
+		$session = $request->getSession();
+		// get the login error if there is one
+		if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+			$error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+		} else {
+			$error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+			$session->remove(SecurityContext::AUTHENTICATION_ERROR);
+		}
+		
+		return $this->render('IdentiteloginBundle:Default:index.html.twig', array(
+		// last username entered by the user
+		'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+		'error'	=> $error,
+		));*/
+    	return $this->indexAction();
+    }
+    
+    public function indexAction(){
+    	return $this->render('IdentiteProfilBundle:Default:index.html.twig');
+    }
+    
+    public function checkAction()
+    {
+    	return $this->indexAction();
+    }
+    
+    public function adminAction()
+    {
+    	return $this->indexAction();
     }
 }
